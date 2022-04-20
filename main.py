905514fedafb206805413f92a1bf71409ec61d12
Mingyou Sung <mingyousung@gmail.com>
2022-04-20 07:54:36 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from word_base_tokenization import word_tokenization
+from character_base_tokenization import character_tokenization
+from subword_base_tokenization import bpe_processing, encoding
+
+sample_text = """My lovely dog likes eating sausage.
+My cat likes eating salmon.
+Jane's koala likes eating eucalyptus leaves.
+Jack's panda likes eating bamboo."""
+
+listofsent = sample_text.split("\n")
+
+idx = random.randint(0,len(listofsent)-1)
+print(listofsent[idx])
+
+# Word-based tokenization
+print(word_tokenization(listofsent[idx], None, True))
+print(word_tokenization(listofsent[idx], "stanza", True))
+print(word_tokenization(listofsent[idx], "spacy", True))
+print(word_tokenization(listofsent[idx], "nltk", True))
+print(word_tokenization(listofsent[idx], "gensim", True))
+
+# Character-based tokenization
+print(character_tokenization(listofsent[idx], None, True))
+
+# Subword-based tokenization
+num_merges = 20
+bpe_codes = bpe_processing(listofsent, num_merges)
+print(encoding(listofsent[idx]))
