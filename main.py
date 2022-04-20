91257f9b5810b395dd1be0c660c24c57de4d573b
Mingyou Sung <mingyousung@gmail.com>
2022-04-20 07:56:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from word_base_tokenization import word_tokenization
 from character_base_tokenization import character_tokenization
 from subword_base_tokenization import bpe_processing, encoding
 
+import random
+
 sample_text = """My lovely dog likes eating sausage.
 My cat likes eating salmon.
 Jane's koala likes eating eucalyptus leaves.
@@ -20,7 +22,7 @@ print(word_tokenization(listofsent[idx], "nltk", True))
 print(word_tokenization(listofsent[idx], "gensim", True))
 
 # Character-based tokenization
-print(character_tokenization(listofsent[idx], None, True))
+print(character_tokenization(listofsent[idx], True))
 
 # Subword-based tokenization
 num_merges = 20
